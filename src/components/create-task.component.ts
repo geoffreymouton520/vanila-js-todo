22250db08e408aa86ba49e0a42ed34507ebb2ceb
geoffreymouton520 <emokid250@gmail.com>
2021-10-30 23:45:41 +0200

    Added webpack

--- src/components/create-task.component.ts
+++ src/components/create-task.component.ts
@@ -8,10 +8,16 @@ export class CreateTaskComponent {
     public init(): void {
         this.title = document.getElementById('create-task-title') as HTMLInputElement;
         this.submitButton = document.getElementById('create-task-submit') as HTMLButtonElement;
-        this.submitButton.addEventListener('click', this.submit);
+        this.submitButton.addEventListener('click', (ev: MouseEvent) => {
+            this.submit(ev);
+        });
     }
 
-    private submit(): void {
+    private submit(ev: MouseEvent): void {
+        ev.preventDefault();
+
+        this.title = document.getElementById('create-task-title') as HTMLInputElement;
         console.log('The form was submitted:', this.title.value);
+        console.log('Title element:', this.title);
     }
 }
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    watch: true,
+    entry: './src/app.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
